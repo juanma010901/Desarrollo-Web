ffb5e0b18e8dc2178080d8a8d693824ff7d8bae5
Juan M <juanmahc0109@gmail.com>
2024-01-17 18:44:59 -0500

    New testing files

--- 05_Intro a Backend + SQL/test/src/main.jsx
+++ 05_Intro a Backend + SQL/test/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App.jsx'
-import MyRoutes from './router';
+import MyRoutes from './router.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 
